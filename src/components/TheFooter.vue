64a73fee0891d947e1da3a77b1e2b92d0b0622a5
z01a <5a6f4c41@gmail.com>
2025-06-27 00:09:45 +0200

    Merge 'dev' into 'main' for v1.0.0

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const currentYear = ref<number>(new Date().getFullYear())
+</script>
+
+<template>
+  <footer>
+    <h6>&copy; {{ currentYear }} <a href="#">z01a.io</a>. No rights reserved.</h6>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+h6 {
+  font-size: 1em;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import TypeWriter from '@/components/TypeWriter.vue'
+import TheFooter from '@/components/TheFooter.vue'
+</script>
+
+<template>
+  <section class="container">
+    <TypeWriter text="z01a" />
+    <TheFooter />
+  </section>
+</template>
+
+<style scoped lang="scss">
+.container {
+  text-align: center;
+}
+</style>
